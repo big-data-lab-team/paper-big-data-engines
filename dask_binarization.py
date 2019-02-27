3e94ec201caf320e162d3087c448b18f6b0a6b8e
Mathieu Dugré <math.dugre@gmail.com>
2019-02-27 15:27:27 -0500

    Created a decorator to benchmark function

---
+++ dask_binarization.py
@@ -0,0 +1,19 @@
+from benchmark import benchmark
+
+
+def read_img(data):
+    pass
+
+
+
+def binarize_data(data, metadata):
+    pass
+
+
+
+def save_binarization(data, metadata):
+    pass
+
+
+def main():
+    pass
