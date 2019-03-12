2a020db46b0c2c38026922644fc1e29e66a25394
Mathieu Dugré <math.dugre@gmail.com>
2019-03-11 23:00:28 -0400

    Works on local machine

--- dask_binarization.py
+++ dask_binarization.py
@@ -1,76 +1,93 @@
 import argparse
 import os
-from io import BytesIO
 import numpy as np
 import nibabel as nib
 import dask.bag as db
+from dask.distributed import Client, LocalCluster
 from utils import benchmark
 
 
-def retrieve_input(input_dir):
-    """
+def crawl_dir(input_dir):
+    """Crawl the input directory to retrieve MINC files.
     
-    :param input_dir:
-    :return:
+    :param input_dir: str -- representation of the path for the input file.
+    :return: list -- of the retrieved path.
     """
     rv = list()
     for folder, subs, files in os.walk(input_dir):
         for filename in files:
             if filename.endswith('.mnc'):
                 path = os.path.join(folder, filename)
-                with open(path, 'rb') as f:
-                    rv.append((path, f.read()))
+                rv.append(path)
     return rv
 
 
-def read_img(filename, is_benchmarking, output_dir, *, data=None):
-    """
+@benchmark()
+def read_img(filename, is_benchmarking, output_dir):
+    """Read the image from an MINC format to a Nifti format.
     
-    :param filename:
-    :param is_benchmarking:
-    :param output_dir:
-    :param data:
-    :return:
+    :param filename: str -- representation of the path for the input file.
+    :param is_benchmarking: boolean -- whether or not the benchmark is  saved.
+    :param output_dir: str -- representation of the path for the output file.
+    :return: tuple -- of the form (filename, data, (image affine,
+    image header)).
     """
-    fh = nib.FileHolder(fileobj=BytesIO(data))
-    img = nib.Nifti1Image.from_file_map({'header': fh, 'image': fh})
+    minc = nib.load(filename)
+    img = nib.Nifti1Image(minc.get_data(), affine=minc.affine)
     
     data = img.get_data()
     
     return (filename, data, (img.affine, img.header))
 
 
+@benchmark(ignore=['data', 'metadata', 'threshold'])
 def binarize_data(filename, is_benchmarking, output_dir, *, data=None,
-                  metadata=None):
-    """
+                  metadata=None, threshold=0.5):
+    """Binarize the data.
     
-    :param filename:
-    :param is_benchmarking:
-    :param output_dir:
-    :param data:
-    :param metadata:
+    :param filename: str -- representation of the path for the input file.
+    :param is_benchmarking: boolean -- whether or not the benchmark is  saved.
+    :param output_dir: str -- representation of the path for the output file.
+    :param data: nifti1Image -- image to binarize.
+    :param metadata: tuple -- of the form (image affine, image header).
+    :param threshold: int --
     :return:
     """
-    pass
+    
+    if data.max() == 1:
+        threshold = 0
+    
+    data = np.where(data > threshold, 1, 0)
+    
     return (filename, data, metadata)
 
 
+@benchmark(ignore=['data', 'metadata'])
 def save_binarization(filename, is_benchmarking, output_dir, *, data=None,
                       metadata=None):
-    """
+    """Save the data into a nifti1Image format.
     
-    :param filename:
-    :param is_benchmarking:
-    :param output_dir:
-    :param data:
-    :param metadata:
+    :param filename: str -- representation of the path for the input file.
+    :param is_benchmarking: boolean -- whether or not the benchmark is  saved.
+    :param output_dir: str -- representation of the path for the output file.
+    :param data: nifti1Image -- image to binarize.
+    :param metadata: tuple -- of the form (image affine, image header).
     :return:
     """
-    # return (out_fn, 'SUCCESS')
-    pass
+    img = nib.Nifti1Image(data, metadata[0], header=metadata[1])
+    
+    bn = os.path.basename(filename[:-3] + 'nii')  # Save in nifti format
+    f_out = os.path.join(output_dir, 'bin-' + bn)
+    nib.save(img, f_out)
+    
+    return (f_out, 'SUCCESS')
 
 
 def main():
+    """
+    
+    :return: void
+    """
     parser = argparse.ArgumentParser(description="BigBrain binarization")
     parser.add_argument('bb_dir', type=str,
                         help=('The folder containing BigBrain NIfTI images'
@@ -83,13 +100,36 @@ def main():
     parser.add_argument('--benchmark', action='store_true',
                         help='benchmark results')
     
-    #args = parser.parse_args()
-    imgRDD = db.from_sequence(retrieve_input(os.path.abspath('test/data'))) \
-        .map(lambda x: read_img(x[0], False, './test', data=x[1]))
-
-    # To test the previous functions
-    imgRDD.compute()
-    pass
+    args = parser.parse_args()
+    
+    # set up local cluster on your laptop
+    cluster = LocalCluster(n_workers=1, diagnostics_port=8788)
+    Client(cluster)
+    
+    # Read images
+    img_rdd = db.from_sequence(crawl_dir(os.path.abspath(args.bb_dir))) \
+        .map(lambda path: read_img(path,
+                                   args.benchmark,
+                                   args.output_dir))
+    
+    # Binarize the image data
+    for i in range(args.iterations):
+        img_rdd = img_rdd.map(lambda x: binarize_data(x[0],
+                                                      args.benchmark,
+                                                      args.output_dir,
+                                                      data=x[1],
+                                                      metadata=x[2],
+                                                      threshold=args.threshold))
+    
+    # Save the binarized data
+    img_rdd = img_rdd.map(lambda x: save_binarization(x[0],
+                                                      args.benchmark,
+                                                      args.output_dir,
+                                                      data=x[1],
+                                                      metadata=x[2]))
+    
+    # Compute above function
+    img_rdd.compute()
 
 
 if __name__ == '__main__':
