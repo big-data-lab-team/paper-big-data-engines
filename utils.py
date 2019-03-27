df50980e0d9ef9afb77ed7955aba1481449a82f8
Mathieu Dugré <math.dugre@gmail.com>
2019-03-27 15:08:11 -0400

    Change the benchmark decorator signature to include the start time of the program and a tag to add to the benchmarking file

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from time import perf_counter
+from time import time
 import os
 import socket
 import uuid
@@ -12,6 +12,8 @@ def benchmark(_func=None, *, ignore=None):
         - filename : path to the input file.
         - is_benchmarking : boolean -- if the benchmark is written to file.
         - output_dir : path where the result are written.
+        - start : time of the experiment
+        - experiment : name of the experiment executing.
     
     All other function arguments must be passed by key-word.
     
@@ -19,28 +21,26 @@ def benchmark(_func=None, *, ignore=None):
     :param ignore: list --  of arguments to ignore when writing to output file.
     :return: value of func_
     """
+    
     def benchmark_wrapper(func):
         @wraps(func)
-        def wrapper(filename, is_benchmarking, output_dir, *args,
-                    **kwargs):
+        def wrapper(filename, is_benchmarking, output_dir, experiment, start,
+                    *args, **kwargs):
             # Calculate run time
-            start_time = perf_counter()
-            value = func(filename, is_benchmarking, output_dir,
-                         *args, **kwargs)
-            end_time = perf_counter()
-            run_time = end_time - start_time
+            start_time = time() - start
+            value = func(filename, is_benchmarking, output_dir, experiment,
+                         start, *args, **kwargs)
+            end_time = time() - start
             
             # Write to file if benchmarking
             if is_benchmarking:
                 benchmark_dir = os.path.join(output_dir, 'benchmarks')
                 os.makedirs(benchmark_dir, exist_ok=True)
                 
-                # TODO Change the uuid to something to will allow tracking of
-                #  multiple related benchmark in the same file.
                 calling_file = func.__globals__['__file__'].split('/')[-1][:-3]
                 benchmark_file = os.path.join(
                     benchmark_dir,
-                    "bench-{}.txt".format(calling_file)
+                    "bench-{}-{}.txt".format(calling_file, experiment)
                 )
                 
                 node = socket.gethostname()
@@ -48,8 +48,9 @@ def benchmark(_func=None, *, ignore=None):
                 
                 with open(benchmark_file, 'a+') as f_out:
                     # Write
-                    f_out.write('{0} {1} {2} {3}'.format(func.__name__,
-                                                         run_time,
+                    f_out.write('{0} {1} {2} {3} {4}'.format(func.__name__,
+                                                         start_time,
+                                                         end_time,
                                                          bn,
                                                          node))
                     # Write optional keyword-arguments
