01b938a684998bf7755b91834a7674c4805cf990
Mathieu Dugré <math.dugre@gmail.com>
2019-04-16 21:50:19 -0400

    Moved the increment functions in a seperate file

--- dask-delayed_incrementation.py
+++ dask-delayed_incrementation.py
@@ -1,100 +1,16 @@
 import argparse
-from time import time, sleep
+from time import time
 import os
 
-import nibabel as nib
-from dask.distributed import Client, LocalCluster, fire_and_forget
 import dask
+from dask.distributed import Client
 
-from utils import benchmark, crawl_dir
-
-
-@dask.delayed
-def read_img(filename, start, args):
-    """Read the image from an MINC format to a Nifti format.
-    
-    :param filename: str -- representation of the path for the input file.
-    :return: tuple -- of the form (filename, data, (image affine,
-    image header)).
-    """
-    start_time = time() - start
-    
-    img = nib.load(filename)
-    data = img.get_data()
-    
-    end_time = time() - start
-    
-    if args.benchmark:
-        benchmark(start_time, end_time, filename, args.output_dir,
-                  args.experiment, 'read_img')
-    
-    return filename, data, (img.affine, img.header)
-
-
-@dask.delayed
-def increment(img_rdd, delay, start, args):
-    """Increment the data of a Nifti image by 1.
-    
-    :param filename: str -- representation of the path for the input file.
-    :param data: nifti1Image -- image to manipulate.
-    :param metadata: tuple -- of the form (image affine, image header).
-    :param delay: int -- sleep time for the task
-    :return: tuple -- of the form (filename, data, (image affine,
-    image header), iteration+1).
-    """
-    start_time = time() - start
-    
-    filename = img_rdd[0]
-    data = img_rdd[1]
-    metadata = img_rdd[2]
-    
-    data += 1
-    sleep(delay)
-    
-    end_time = time() - start
-    
-    if args.benchmark:
-        benchmark(start_time, end_time, filename, args.output_dir,
-                  args.experiment, 'increment')
-    
-    return filename, data, metadata
-
-
-@dask.delayed
-def save_incremented(img_rdd, start, args):
-    """Save a Nifti image.
-    
-    :param filename: str -- representation of the path for the input file.
-    :param data: nifti1Image -- image to manipulate.
-    :param metadata: tuple -- of the form (image affine, image header).
-    :return: tuple -- of the form (f_out, 'SUCCESS')
-    """
-    start_time = time() - start
-    
-    filename = img_rdd[0]
-    data = img_rdd[1]
-    metadata = img_rdd[2]
-    
-    bn = os.path.basename(filename[:-3] + 'nii')  # Save in nifti format
-    f_out = os.path.join(args.output_dir, 'images/'+bn)
-    
-    img = nib.Nifti1Image(data, metadata[0], header=metadata[1])
-    nib.save(img, f_out)
-    
-    end_time = time() - start
-    
-    if args.benchmark:
-        benchmark(start_time, end_time, filename, args.output_dir,
-                  args.experiment, 'save_incremented')
-    
-    return f_out, 'SUCCESS'
+from utils import crawl_dir
+from Increment import read_img, increment, save_incremented
 
 
 if __name__ == '__main__':
-    """Execute the incrementation of a Nifti image's data.
     
-    :return:
-    """
     start = time()  # Start time of the pipeline
     
     parser = argparse.ArgumentParser(description="BigBrain incrementation")
@@ -128,15 +44,19 @@ if __name__ == '__main__':
     
     results = []
     for path in paths:
-        img_rdd = read_img(path, start=start, args=args)
+        img_rdd = dask.delayed(read_img(path, start=start, args=args))
         
         # Increment the data n time:
         for _ in range(args.iterations):
-            img_rdd = increment(img_rdd, delay=args.delay, start=start,
-                                args=args)
+            img_rdd = dask.delayed(increment(img_rdd,
+                                             delay=args.delay,
+                                             start=start,
+                                             args=args))
         
         # Save the data
-        results.append(save_incremented(img_rdd, start=start, args=args))
+        results.append(dask.delayed(save_incremented(img_rdd,
+                                                     start=start,
+                                                     args=args)))
     
     client.scatter(results)
     futures = client.compute(results)
--- dask-futures_incrementation.py
+++ dask-futures_incrementation.py
@@ -1,97 +1,15 @@
 import argparse
-from time import time, sleep
+from time import time
 import os
 
-import nibabel as nib
-from dask.distributed import Client, LocalCluster, fire_and_forget
-import dask
+from dask.distributed import Client, fire_and_forget
 
-from utils import benchmark, crawl_dir
-
-
-def read_img(filename, start, args):
-    """Read the image from an MINC format to a Nifti format.
-    
-    :param filename: str -- representation of the path for the input file.
-    :return: tuple -- of the form (filename, data, (image affine,
-    image header)).
-    """
-    start_time = time() - start
-    
-    img = nib.load(filename)
-    data = img.get_data()
-    
-    end_time = time() - start
-    
-    if args.benchmark:
-        benchmark(start_time, end_time, filename, args.output_dir,
-                  args.experiment, 'read_img')
-    
-    return filename, data, (img.affine, img.header)
-
-
-def increment(img_rdd, delay, start, args):
-    """Increment the data of a Nifti image by 1.
-    
-    :param filename: str -- representation of the path for the input file.
-    :param data: nifti1Image -- image to manipulate.
-    :param metadata: tuple -- of the form (image affine, image header).
-    :param delay: int -- sleep time for the task
-    :return: tuple -- of the form (filename, data, (image affine,
-    image header), iteration+1).
-    """
-    start_time = time() - start
-    
-    filename = img_rdd[0]
-    data = img_rdd[1]
-    metadata = img_rdd[2]
-    
-    data += 1
-    sleep(delay)
-    
-    end_time = time() - start
-    
-    if args.benchmark:
-        benchmark(start_time, end_time, filename, args.output_dir,
-                  args.experiment, 'increment')
-    
-    return filename, data, metadata
-
-
-def save_incremented(img_rdd, start, args):
-    """Save a Nifti image.
-    
-    :param filename: str -- representation of the path for the input file.
-    :param data: nifti1Image -- image to manipulate.
-    :param metadata: tuple -- of the form (image affine, image header).
-    :return: tuple -- of the form (f_out, 'SUCCESS')
-    """
-    start_time = time() - start
-    
-    filename = img_rdd[0]
-    data = img_rdd[1]
-    metadata = img_rdd[2]
-    
-    bn = os.path.basename(filename[:-3] + 'nii')  # Save in nifti format
-    f_out = os.path.join(args.output_dir, 'images/'+bn)
-    
-    img = nib.Nifti1Image(data, metadata[0], header=metadata[1])
-    nib.save(img, f_out)
-    
-    end_time = time() - start
-    
-    if args.benchmark:
-        benchmark(start_time, end_time, filename, args.output_dir,
-                  args.experiment, 'save_incremented')
-    
-    return f_out, 'SUCCESS'
+from utils import crawl_dir
+from Increment import read_img, increment, save_incremented
 
 
 if __name__ == '__main__':
-    """Execute the incrementation of a Nifti image's data.
     
-    :return:
-    """
     start = time()  # Start time of the pipeline
     
     parser = argparse.ArgumentParser(description="BigBrain incrementation")
