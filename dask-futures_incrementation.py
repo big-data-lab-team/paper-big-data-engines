6f0980d34c3a3c950dab83dfe13ae01870de5556
Mathieu Dugré <math.dugre@gmail.com>
2019-04-16 23:48:09 -0400

    fixed import for workers

--- dask-futures_incrementation.py
+++ dask-futures_incrementation.py
@@ -36,8 +36,8 @@ if __name__ == '__main__':
     client = Client(cluster)
     
     print(client)
-    client.upload_file('Increment.py')
     client.upload_file('utils.py')  # Allow workers to use module
+    client.upload_file('Increment.py')
     
     # Read images
     paths = crawl_dir(os.path.abspath(args.bb_dir))
