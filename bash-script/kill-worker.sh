f9def47b4eacce1e926b32804e711fc6cbc9c5ee
Mathieu Dugré <math.dugre@gmail.com>
2019-05-13 11:30:32 -0400

    Changed the scheduler ip

--- bash-script/kill-worker.sh
+++ bash-script/kill-worker.sh
@@ -3,5 +3,5 @@ while read h
 do
     echo 'Killing worker on '$h
     ssh -f $h 'screen -S worker -X quit'
-done < host.txt
+done < host16.txt
 echo 'DONE'
--- bash-script/setup-worker.sh
+++ bash-script/setup-worker.sh
@@ -2,6 +2,6 @@
 while read h
 do
     ssh $h &>/dev/null << 'EOF'
-    sudo mount 192.168.73.23:/nfs /nfs
+    sudo mount 192.168.73.10:/nfs /nfs
 EOF
-done < host.txt
+done < host16.txt
