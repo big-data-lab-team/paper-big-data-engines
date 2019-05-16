d7dc2073aab007ede27497d12571c93db1b5df60
Mathieu Dugré <math.dugre@gmail.com>
2019-05-16 11:12:18 -0400

    Moved save_results to Increment.py

--- incrementation/Increment.py
+++ incrementation/Increment.py
@@ -2,7 +2,10 @@ import sys
 
 sys.path.append("/nfs/SOEN-499-Project")
 
+import os
 from time import time, sleep
+
+import nibabel as nib
 
 from utils import benchmark
 
@@ -39,3 +42,45 @@ def increment(img_rdd, delay, start, args):
         )
 
     return filename, data, metadata
+
+
+def save_results(img_rdd, start, args):
+    """Save a Nifti image.
+
+    Parameters
+    ----------
+    img_rdd: (str, np.array, (np.array, np.array))
+        Filename, image, and image header and affine.
+
+    Returns
+    -------
+    f_out : str
+        Output path where the image is saved.
+    "SUCCESS" : str
+        Indicates that the pipeline succeeded.
+    """
+    start_time = time() - start
+
+    filename = img_rdd[0]
+    data = img_rdd[1]
+    metadata = img_rdd[2]
+
+    bn = os.path.basename(filename[:-3] + "nii")  # Save in nifti format
+    f_out = os.path.join(args.output_dir, "images/" + bn)
+
+    img = nib.Nifti1Image(data, metadata[0], header=metadata[1])
+    nib.save(img, f_out)
+
+    end_time = time() - start
+
+    if args.benchmark:
+        benchmark(
+            start_time,
+            end_time,
+            filename,
+            args.output_dir,
+            args.experiment,
+            save_results.__name__,
+        )
+
+    return f_out, "SUCCESS"
