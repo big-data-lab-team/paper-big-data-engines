bc7869e4eac3f39ab64b8a8ae4b917006dcaf4cd
mathdugre <mathdugre@pm.me>
2021-06-01 15:21:44 -0400

    [REMOVE] cleanup scripts from previous paper

--- bash-script/init-worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read h
-do
-    echo 'Initialising worker on '$h
-    ssh -f $h 'screen -S worker -dm dask-worker 192.168.73.10:8786'
-done < host4.txt
-echo 'DONE'
--- bash-script/kill-worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read h
-do
-    echo 'Killing worker on '$h
-    ssh -f $h 'screen -S worker -X quit'
-done < host8.txt
-echo 'DONE'
